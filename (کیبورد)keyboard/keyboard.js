5b54cb71ed911078c13c43fcb6555ad1903167ea
mahdi-mohammadi-16 <141010508+mahdi-mohammadi-16@users.noreply.github.com>
2025-03-05 18:49:46 +0330

    Add files via upload

---
+++ (کیبورد)keyboard/keyboard.js
@@ -0,0 +1,295 @@
+let button = document.getElementById('button');
+let p = document.querySelector('p');
+
+
+
+
+button.addEventListener('keydown',(event)=>{
+})
+//------------------------------AA--------------------------------
+
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'a'){
+        document.getElementById('a').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'a'){
+        document.getElementById('a').classList.remove("lk");
+    }
+})
+//------------------------------SS------------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 's'){
+        document.getElementById('s').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 's'){
+        document.getElementById('s').classList.remove("lk");
+    }
+})
+//------------------------------DD----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'd'){
+        document.getElementById('d').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'd'){
+        document.getElementById('d').classList.remove("lk");
+    }
+})
+//------------------------------FF----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'f'){
+        document.getElementById('f').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'f'){
+        document.getElementById('f').classList.remove("lk");
+    }
+})
+//------------------------------GG----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'g'){
+        document.getElementById('g').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'g'){
+        document.getElementById('g').classList.remove("lk");
+    }
+})
+//------------------------------HH----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'h'){
+        document.getElementById('h').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'h'){
+        document.getElementById('h').classList.remove("lk");
+    }
+})
+//------------------------------JJ----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'j'){
+        document.getElementById('j').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'j'){
+        document.getElementById('j').classList.remove("lk");
+    }
+})
+//------------------------------KK----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'k'){
+        document.getElementById('k').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'k'){
+        document.getElementById('k').classList.remove("lk");
+    }
+})
+//------------------------------LL----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'l'){
+        document.getElementById('l').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'l'){
+        document.getElementById('l').classList.remove("lk");
+    }
+})
+//------------------------------QQ----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'q'){
+        document.getElementById('q').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'q'){
+        document.getElementById('q').classList.remove("lk");
+    }
+})
+//------------------------------WW----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'w'){
+        document.getElementById('w').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'w'){
+        document.getElementById('w').classList.remove("lk");
+    }
+})
+//------------------------------EE----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'e'){
+        document.getElementById('e').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'e'){
+        document.getElementById('e').classList.remove("lk");
+    }
+})
+//------------------------------RR----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'r'){
+        document.getElementById('r').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'r'){
+        document.getElementById('r').classList.remove("lk");
+    }
+})
+//------------------------------TT----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 't'){
+        document.getElementById('t').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 't'){
+        document.getElementById('t').classList.remove("lk");
+    }
+})
+//------------------------------YY----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'y'){
+        document.getElementById('y').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'y'){
+        document.getElementById('y').classList.remove("lk");
+    }
+})
+//------------------------------UU----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'u'){
+        document.getElementById('u').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'u'){
+        document.getElementById('u').classList.remove("lk");
+    }
+})
+//------------------------------II----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'i'){
+        document.getElementById('i').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'i'){
+        document.getElementById('i').classList.remove("lk");
+    }
+})
+//------------------------------OO----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'o'){
+        document.getElementById('o').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'o' ){
+        document.getElementById('o').classList.remove("lk");
+    }
+})
+//------------------------------PP----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'p'){
+        document.getElementById('p').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'p'){
+        document.getElementById('p').classList.remove("lk");
+    }
+})
+//------------------------------ZZ----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'z'){
+        document.getElementById('z').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'z'){
+        document.getElementById('z').classList.remove("lk");
+    }
+})
+//------------------------------XX----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'x'){
+        document.getElementById('x').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'x'){
+        document.getElementById('x').classList.remove("lk");
+    }
+})
+//------------------------------CC----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'c'){
+        document.getElementById('c').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'c'){
+        document.getElementById('c').classList.remove("lk");
+    }
+})
+//------------------------------VV----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'v'){
+        document.getElementById('v').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'v'){
+        document.getElementById('v').classList.remove("lk");
+    }
+})
+//------------------------------BB----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'b'){
+        document.getElementById('b').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'b'){
+        document.getElementById('b').classList.remove("lk");
+    }
+})
+//------------------------------NN----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'n'){
+        document.getElementById('n').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'n'){
+        document.getElementById('n').classList.remove("lk");
+    }
+})
+//------------------------------MM----------------------------
+button.addEventListener('keydown', (event)=>{
+    if(event.key == 'm'){
+        document.getElementById('m').classList.add("lk");
+    }
+})
+button.addEventListener('keyup', (event)=>{
+    if(event.key == 'm'){
+        document.getElementById('m').classList.remove("lk");
+    }
+})
